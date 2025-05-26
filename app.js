be11b9d9526d751d83cc4b55b3397865d190c4c8
KemiA <kzagbabiaka@gmail.com>
2025-05-26 13:43:53 -0400

    update

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files (like index.html) from the current directory
+app.use(express.static(__dirname));
+
+// WebSocket logic
 io.on('connection', socket => {
     console.log('User connected');
     socket.on('disconnect', () => console.log('User disconnected'));
 });
 
+// Start server
 server.listen(3000, () => console.log('Chat app running on port 3000'));
