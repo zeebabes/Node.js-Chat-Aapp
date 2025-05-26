089bd4578005c3354c4ad6172085fd7ea71daa7e
KemiA <kzagbabiaka@gmail.com>
2025-05-26 19:12:58 -0400

    update

--- app.js
+++ app.js
@@ -7,31 +7,23 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files
 app.use(express.static(__dirname));
-
-const users = {};
 
 io.on('connection', socket => {
   console.log('User connected:', socket.id);
-  users[socket.id] = socket;
 
-  // When user sends a message
   socket.on('chatMessage', msg => {
     console.log(`User ${socket.id}: ${msg}`);
-    // Echo to admin interface or others
-    socket.broadcast.emit('chatMessage', `User: ${msg}`);
+    io.emit('chatMessage', `User: ${msg}`);
   });
 
-  // When admin sends a reply
-  socket.on('adminReply', reply => {
-    console.log(`Admin: ${reply}`);
-    socket.broadcast.emit('chatMessage', `Admin: ${reply}`);
+  socket.on('adminReply', msg => {
+    console.log(`Admin: ${msg}`);
+    io.emit('chatMessage', `Admin: ${msg}`);
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
-    delete users[socket.id];
   });
 });
 
